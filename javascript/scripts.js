42587ac83f22f38526c6743f1f67081810c63657
anaCaceres15 <75796701+anaCaceres15@users.noreply.github.com>
2021-04-17 12:05:20 -0500

    dayevska

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,19 @@
-//Place your javascript code related with HTML and CSS here
+$(document).ready(main);
+var contador=1;
+
+funtion main(){
+  $('.menu').click(funtion(){
+      if(contador==1){
+          $('nav').animate({
+             left:'0'
+          });
+          contador=0;
+      }
+      else{
+          contador=1;
+          $('nav').animate({
+             left:'-100%'
+          });
+      }
+  });
+}
