04c39099bc219bcc3c50fae9bb2852a256dfb207
Gabriel Valdivia <vgabriel.gm37@gmail.com>
2021-04-16 20:44:43 -0500

    funciones de JS y avance inicial de diseño

--- javascript/app.js
+++ javascript/app.js
@@ -1,43 +1,57 @@
 // Param numbers: array of numbers
 // Returns a sorted array
+
 function insertion_sort(numbers) {
 
-    //Write your code here
+    let n = numbers.length;
+        for (let i = 1; i < n; i++) {
+            let j = i-1; 
+            while ((j > -1) && (numbers[i] < numbers[j])) {
+                numbers[j+1] = numbers[j];
+                j--;
+            }
+            numbers[j+1] = numbers[i];
+        }
+    return numbers;
 
-    return [-1];
 }
-
 
 // Param number: single integer
 // Returns a single integer
 function factorial(number) {
 
-    //Write your code here
-
-    return -1;
+    var total = 1; 
+	for (i=1; i<=number; i++) {
+		total = total * i; 
+	}
+	return total; 
 }
 
 // Param number: single integer
 // Returns a single integer
 function fibonacci(number) {
-
-    //Write your code here
-
-    return -1
+    let number=[0,1];
+    for (let i = 2; i < number; i++) {
+        number[i] = number[i - 2] + number[i - 1];
+    }
+    return number;
 }
 
 // Param expression: string containing a mathematical expression
 // Returns an integer value
 function evaluation(expression) {
+    var x = new String("2");
+    var y = new String("5");
 
-    //Write your code here
+    var z = x+y;
+    expression.charAt(1);
 
-    return -1
+    return z
 }
 
 
-if(typeof process === 'object') {
-    module.exports =  {
+if (typeof process === 'object') {
+    module.exports = {
         insertion_sort,
         factorial,
         fibonacci,
