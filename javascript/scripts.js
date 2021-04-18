52093e55175f17ae5dd17e8c6fe52c6228a11c4f
Gabriel Valdivia <vgabriel.gm37@gmail.com>
2021-04-17 21:23:26 -0500

    Limpieza scripts

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,19 +1,1 @@
-$(document).ready(main);
-var contador=1;
 
-funtion main(){
-  $('.menu').click(funtion(){
-      if(contador==1){
-          $('nav').animate({
-             left:'0'
-          });
-          contador=0;
-      }
-      else{
-          contador=1;
-          $('nav').animate({
-             left:'-100%'
-          });
-      }
-  });
-}
